72f783e9c3e7a151e33d3e3bc9d705534e169469
katherinevivianw <katherinevivianw@gmail.com>
2023-03-05 14:38:12 -0800

    completed project

--- src/calculate_win.js
+++ src/calculate_win.js
@@ -1,4 +1,37 @@
 export function calculateWinner(squares) {
-  // TODO: Given a game board, calculate the winner.
-  // If there is no winner, you can return null.
+  if (squares[0] && squares[0] === squares[1] && squares[1] === squares[2]) {
+    return squares[0];
+  }
+
+  else if (squares[3] && squares[3] === squares[4] && squares[4] === squares[5]) {
+    return squares[3];
+  }
+
+  else if (squares[6] && squares[6] === squares[7] && squares[7] === squares[8]) {
+    return squares[6];
+  }
+
+  // check vertical wins
+  else if (squares[0] && squares[0] === squares[3] && squares[3] === squares[6]) {
+    return squares[0];
+  }
+
+  else if (squares[1] && squares[1] === squares[4] && squares[4] === squares[7]) {
+    return squares[1];
+  }
+
+  else if (squares[2] && squares[2] === squares[5] && squares[5] === squares[8]) {
+    return squares[2];
+  }
+
+  // check diagonals
+  else if (squares[0] && squares[0] === squares[4] && squares[4] === squares[8]) {
+    return squares[0];
+  }
+
+  else if (squares[2] && squares[2] === squares[4] && squares[4] === squares[6]) {
+    return squares[2];
+  }
+
+  return null;
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,14 +3,70 @@ import { calculateWinner } from "../calculate_win";
 import Board from "./Board";
 
 const Game = () => {
-  // TODO: Set up states and functions: position of Xs and Os on board,
-  // step number, whether X is next, is there a win or tie, etc.
+  // initializing states
+  const boardArr = new Array(9).fill(null);
+  const [board, setBoard] = useState(boardArr);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+
+  // initializing other vars
+  let winner = calculateWinner(board);
+
+  function getPlayer(xIsNext) {
+    let currPlayer = null;
+    if (xIsNext) {
+      currPlayer = "X";
+    } else {
+      currPlayer = "O";
+    }
+    return currPlayer;
+  }
+  let player = getPlayer(xIsNext);
+
+  // initialize functions
+  function handleClick(index) {
+    if (winner === null && board[index] === null) {
+      let currBoard = [...board]; // update board
+      currBoard[index] = player;
+    
+      setXisNext(!xIsNext);
+      setStepNumber(stepNumber + 1); // update stepnumber
+      setBoard(currBoard);
+      winner = calculateWinner(currBoard);
+    }
+  }
+
+  let jumpToStart = () => {
+    let emptyArr = new Array(9).fill(null);
+    setBoard(emptyArr); // reset board
+    player = "X";
+    setStepNumber(0);
+    setXisNext(true);
+  }
+
+  let getResult = (winner, player, stepNumber) => {
+    if (winner !== null) {
+      return "Winner: " + winner +"!";
+    } 
+    if (stepNumber === 9) {
+      return "Tie Game."
+    }
+    return "Next Player: " + player;
+  }
+
+  let result = getResult(winner, player, stepNumber);
 
   return (
     <>
-      TODO: Render the board here along with the title, game status,
-      and 'Go to Start' button.
-    </>
+      <h1>Tic Tac Toe</h1>
+      <Board squares={board} onClick={handleClick} />
+      <div className='info-wrapper'>
+          <div>
+            <button className="button" onClick={jumpToStart}>Go to Start</button>
+          </div>
+          <h3>{result}</h3>
+      </div>
+  </>
   );
 };
 
